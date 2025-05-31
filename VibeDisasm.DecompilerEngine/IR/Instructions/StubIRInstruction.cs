6a9932001ff6eaa71e996f28688f51be8f98ec5c
bird_egop <sampletext32@bk.ru>
2025-05-31 14:28:37 +0300

    move StubIRInstruction to separate file

---
+++ VibeDisasm.DecompilerEngine/IR/Instructions/StubIRInstruction.cs
@@ -0,0 +1,17 @@
+﻿using VibeDisasm.DecompilerEngine.IR.Expressions;
+using VibeDisasm.DecompilerEngine.IR.Instructions;
+using VibeDisasm.Disassembler.X86;
+
+public sealed class StubIRInstruction : IRInstruction
+{
+    private readonly InstructionType _instructionType;
+    public override IRExpression? Result => null;
+    public override IReadOnlyList<IRExpression> Operands => [];
+
+    public StubIRInstruction(InstructionType instructionType)
+    {
+        _instructionType = instructionType;
+    }
+
+    public override string ToString() => $"StubIRInstruction - {_instructionType:G} Not implemented";
+}
